70b3e3129e904d282eae9acf00cdb83a62d73f6e
RKPinata <teukutiga@gmail.com>
2022-09-14 09:30:07 +0800

    update favicon and logo

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,14 @@
 import Link from 'next/link';
+import Image from 'next/image' //this was added by 
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      {/* <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" /> */} {/*remove old styling(logo)*/} 
+      <div className="container w-10 h-10 rounded-full block mx-auto overflow-hidden mb-4 shadow-xl">
+      <Image className="rounded-full mx-auto" src="/logo.png" height={50} width={50} />
+      <div/>
+      </div>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
--- components/SEO.js
+++ components/SEO.js
@@ -4,6 +4,7 @@ export default function SEO({ title, description }) {
   return (
     <Head>
       <title>{title}</title>
+      <link rel="icon" href="/favicon.ico" />
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
     </Head>
